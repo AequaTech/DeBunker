81c8a4c9220eabda4e799eb205192f8f811a8ec2
simonasnow <simona.frenda@gmail.com>
2023-04-05 18:40:33 +0200

    updated with inference from HS/Sarc model

--- go_descriptive_analyses.py
+++ go_descriptive_analyses.py
@@ -1,8 +1,9 @@
 import sys
-sys.path.append('./aequa-tech_packages/') 
-from analysis import affective_analyses
+sys.path.append('./aequaTech_packages/') 
 from dataset import preprocess_dataset
+from analysis import affective_analyses
 from text import preprocess
+from inference import HS_Sarcasm_Inference as hs_sarc
 
 from nltk.tokenize import sent_tokenize,RegexpTokenizer
 import yaml
@@ -10,7 +11,7 @@ import pandas as pd
 import time
 with open(sys.argv[1], encoding='utf8') as f:
     params = yaml.load(f, Loader=yaml.FullLoader)
-    
+
 def descriptive_analyses(sents):    
     list_dicts = []
     for sent in sents:
@@ -18,6 +19,7 @@ def descriptive_analyses(sents):
         
         try:
             d = dict()
+            d['date'] = sent['date']
             d['doc_id'] = sent['doc_id']
             d['sent_id'] = sent['id']
             d['sent'] = sent['text']
@@ -29,6 +31,10 @@ def descriptive_analyses(sents):
             # print('sentiment')
             d['positive'] = sentiment_analysis.sentiment_by_sent(lemmatized_txt)['positive']
             d['negative'] = sentiment_analysis.sentiment_by_sent(lemmatized_txt)['negative']
+            
+            hs, sarc, logit1, logit2 = inference_hs_sarc.prediction(sent['text'])
+            d['hs'] = hs
+            d['sarcasm'] = sarc
             
             # print('emotions')
             d_emo = emotion_analysis.emotions_by_sent(lemmatized_txt)
@@ -46,6 +52,8 @@ def descriptive_analyses(sents):
     
 start = time.time()
 
+inference_hs_sarc = hs_sarc.Predicting()
+
 preprocess = preprocess.PreprocessingSpacy(params['language'][0])
 
 sentiment_analysis = affective_analyses.Sentix()
@@ -54,22 +62,26 @@ emotion_analysis = affective_analyses.Emotions_NRC(params['language'][0])
 
 texts = []
 ids = []
+dates = []
 
 if params['data']['sentences'] != None:
     texts.extend(params['data']['sentences'])
     ids.extend([i for i in range(len(texts))])
+    dates.extend([i for i in range(len(texts))])
 else: 
     path = params['data']['input_file']
     df = preprocess_dataset.PrepareDF(params['data']['source'][0]).call_df(path)
     texts.extend(df['text'].tolist())
     ids.extend(df['id'].tolist())
+    dates.extend(df['date'].tolist())
 
 sents=[]
-for idx, txt in zip(ids, texts):
-    sents.extend([{'doc_id': idx, 'text':' '.join(RegexpTokenizer(r'\w+').tokenize(x)), 'id':i} for i,x in enumerate(sent_tokenize(txt))])
+for idx, txt, date in zip(ids, texts, dates):
+    sents.extend([{'date': date, 'doc_id': idx, 'text':' '.join(RegexpTokenizer(r'\w+').tokenize(x)), 'id':i} for i,x in enumerate(sent_tokenize(txt))])
 print('length data: ', len(sents))
 
 df_output = descriptive_analyses(sents)
+
 finish = time.time()
 # print(finish)
 
