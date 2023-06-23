47a5ab665925a89910389666b7b300c064cf545b
AequaTech <aequa.tech@gmail.com>
2023-06-23 14:45:04 +0200

    Delete initialize.py

--- API/initialize.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-con = sqlite3.connect("dubunkerAPI.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE  IF NOT EXISTS urls(hash_id CHAR PRIMARY KEY, url CHAR UNIQUE, title TEXT, content TEXT, date DATE KEY)")
-con.close()
