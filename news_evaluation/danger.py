a19352695ee7edb0aea47fd7f465164f151ef6ea
marcostranisci <marcoantonio.stranisci@unito.it>
2023-06-24 16:13:07 +0200

    fixed model and added requests.txt

--- news_evaluation/danger.py
+++ news_evaluation/danger.py
@@ -1,7 +1,8 @@
 from transformers import AutoConfig,AutoModel,AutoTokenizer
 import torch.nn as nn
-import dlmodel.predictors as predictors
+import torch
 from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
 
 
 
@@ -11,23 +12,37 @@ class Danger():
     def __init__(self,model_name: str = None, num_classes: int = None,stereo_cp=None,irony_cp=None,fake_cp=None,flame_cp=None):
         self.model_name = model_name
 
-        self.model = predictors.BertBasedPrediction(model_name, num_classes)
-        
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=2)
+
+
 
 
     def stereotype_detection(self,title: str = None, req_id: str = None):
         tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        tokenized = tokenizer.encode_plus(title,req_id,return_tensors='pt')
+        tokenized = tokenizer.encode_plus(title,return_tensors='pt')
 
         feat = tokenized['input_ids']
         attention = tokenized['attention_mask']
-        
+
         model = self.model
 
-        model.load_state_dict(torch.load('state_dict.pth'))
+        #model.load_state_dict(torch.load('state_dict.pth'))
 
         model.eval()
 
-        result = model(features=feat,attention_mask=attention)
+        result = model(input_ids=feat,attention_mask=attention)
 
         return result
+
+
+def main():
+    model = Danger('distilbert-base-uncased',2)
+    req = 123
+    s = 'I hate immigrants!'
+    res = model.stereotype_detection(title=s,req_id=req)
+    print(res)
+    return res
+
+if __name__=='__main__':
+
+    main()
