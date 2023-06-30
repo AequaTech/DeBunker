d525b3ee345b0c1f58b423caa60ef33f6936a4a1
marcostranisci <marcoantonio.stranisci@unito.it>
2023-06-30 11:48:40 +0200

    added  tokenizer

--- API/news_evaluation/danger.py
+++ API/news_evaluation/danger.py
@@ -1,38 +1,41 @@
 from transformers import AutoConfig,AutoModel,AutoTokenizer
 import torch.nn as nn
-import torch
+import torch,sys
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from safetensors.torch import load_model, load_file
+sys.path.insert(1,'../webScraper/')
+
+from WebScraper import BertBasedTokenizer
 
 class Danger():
 
-    def __init__(self,model_name: str = None, num_classes: int = None, task='flame'):
+    def __init__(self,model_name: str = None, num_classes: int = None,cp: str = None, task='flame'):
                  # stereo_cp=None,irony_cp=None,fake_cp=None,flame_cp=None):
         self.model_name = model_name
 
         self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=num_classes)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        
+        self.cp = cp
+        '''
         if task == 'stereo':
-            self.cp = '\DeBunker\API\resources\models\stereo.safetensors' 
+            self.cp = '\DeBunker\API\resources\models\stereo.safetensors'
         elif task == 'flame':
             self.cp = r"\DeBunker\API\resources\models\hs.safetensors"
         elif task == 'iro':
-            self.cp = '\DeBunker\API\resources\models\iro.safetensors' 
+            self.cp = '\DeBunker\API\resources\models\iro.safetensors'
         else:
             self.cp = '\DeBunker\API\resources\models\sarcasm.safetensors' #sarcasm
+        '''
 
-
-    def classification(self,title: str = None):#, req_id: str = None): si occupa fastapi del caching
-        tokenized = self.tokenizer.encode_plus(title,return_tensors='pt')
-
-        feat = tokenized['input_ids']
-        attention = tokenized['attention_mask']
+    def classification(self,feat,attention):
 
         model = self.model
-
-        model.load_state_dict(load_file(self.cp))
+        cp = self.cp
+        if cp is not None:
+            model.load_state_dict(load_file(self.cp))
+        else:
+            model
 
         model.eval()
 
@@ -47,9 +50,12 @@ def main():
     model = Danger('dbmdz/bert-base-italian-cased', 2, task='flame')
     req = 123
     # s = 'I hate immigrants!'
-    s = 'i musulmano ammazzano tutti quelli che per il loro fumoso cervello sono " infedeli " . i nostri terroristi istituzionali ci obbligano ad accogliere e mantenere i nostri assassini'
-    res = model.classification(title=s) #,req_id=req)si occupa fastapi del caching
+    s = 'i musulmani ammazzano tutti quelli che per il loro fumoso cervello sono " infedeli " . i nostri terroristi istituzionali ci obbligano ad accogliere e mantenere i nostri assassini'
+    tokenizer = BertBasedTokenizer('dbmdz/bert-base-italian-cased')
+    feat,attention = tokenizer.tokenize_text(s)
+    res = model.classification(feat,attention) #,req_id=req)si occupa fastapi del caching
     #print(torch.argmax(res['logits']).item())
+    print(res)
     return res
 
 if __name__=='__main__':
